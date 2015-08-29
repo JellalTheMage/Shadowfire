0b13d47cab8655fece031302a726dec0dc9e1990
Steelchar <sundarshanmugavel@yahoo.com>
2015-08-29 09:53:20 -0700

    Update misc.js

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -1,3 +1,4 @@
+
 /**
  * Miscellaneous commands
  */
@@ -68,6 +69,13 @@ exports.commands = {
 				Users.get(users[len]).joinRoom(room, Users.get(users[len]).connections[0]);
 			}
 		}, 1000);
+	},
+
+	hide: function (target, room, user) {
+		if (!this.can('lock')) return false;
+		user.hiding = true;
+		user.updateIdentity();
+		this.sendReply("You have hidden your staff symbol.");
 	},
 
 	rk: 'kick',
@@ -184,6 +192,11 @@ exports.commands = {
 	},
 	regdatehelp: ["/regdate - Please specify a valid username."],
 
+	show: function (target, room, user) {
+		if (!this.can('lock')) return false;
+		user.hiding = false;
+		user.updateIdentity();
+		this.sendReply("You have revealed your staff symbol.");
 	},
 
 	sb: 'showdownboilerplate',
